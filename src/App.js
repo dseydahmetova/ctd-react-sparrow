f941a4e418a9b8e5182d3d16e4de11a9c8a9827f
dseydahmetova <dseydahmetova@gmail.com>
2022-04-22 19:38:31 -0400

    Created custom Hook and used fragment

--- src/App.js
+++ src/App.js
@@ -15,19 +15,30 @@ import AddTodoForm from "./AddTodoForm"
   )
   }*/
 
+const useSemiPersistentState = () => {
+  const [todoList, setTodoList] = React.useState(JSON.parse(localStorage.getItem('savedTodoList')) || [])
+
+  React.useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList]);
+
+  return [todoList, setTodoList];
+
+}
+
+
 function App() {
-  const [todoList, setTodoList] = React.useState([]);
+  const [todoList, setTodoList] = useSemiPersistentState();
   const addTodo = (newTodo) => {
     setTodoList([...todoList, newTodo])
   }
   return (
-    <div>
+    <>
       <h1>To do List for CTD</h1>
       <AddTodoForm onAddTodo={addTodo} />
       <TodoList todoList={todoList} />
-    </div>
+    </>
   );
 }
-
 
 export default App;
